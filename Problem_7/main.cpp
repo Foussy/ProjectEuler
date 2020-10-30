8c450b29605e2ff2931eb1c94f127fcc9187bc4c
Christian Hiricoiu <christian.hiricoiu@gmail.com>
2020-10-30 17:21:29 +0100

    Update main.cpp

--- Problem_7/main.cpp
+++ Problem_7/main.cpp
@@ -1,8 +1,6 @@
 /*
 By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
-
 What is the 10 001st prime number?
-
 answer :
 */
 
@@ -10,21 +8,27 @@ answer :
 
 bool isPrime(int n)
 {
-
+    for(int i=2,i<n,i++)
+    {
+        if (n % i == 0)
+        {
+            return false;
+        }
+    }
+    return true;
 }
 
 int umpteenthPrime(int n)
 {
-    int primeList = {};
+    list<int> primeList;
     int i=2;
     do
     {
         if (isPrime(i) == true)
         {
-            
-        }
-        
-    } while ();
+            primeList.pushback(i);
+        }        
+    } while (primeList.size()<10001);
     
 }
 
