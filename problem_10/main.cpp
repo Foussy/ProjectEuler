165a12cdbd083553032b0e0087d6a39dc8a88fcc
Christian Hiricoiu <christian.hiricoiu@gmail.com>
2020-11-05 18:58:20 +0100

    update

--- problem_10/main.cpp
+++ problem_10/main.cpp
@@ -3,22 +3,26 @@ The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
 
 Find the sum of all the primes below two million.
 
-answer :
+answer : 142913828922
 */
 
 #include<iostream>
+#include<vector>
+#include<numeric>
+
+std::vector <int> primeList;
 
 bool isPrime(int n)
 {
     int flag = 0;
-    for (int i = 2; i < n; i++)
+    for (auto prime : primeList)
     {
-        if (n % i == 0)
+        if(n % prime == 0)
         {
             flag++;
             break;
         }
-    }        
+    }
     if (flag != 0)
     {
         return false;
@@ -31,22 +35,28 @@ bool isPrime(int n)
 
 long long int sumPrimes(int n)
 {
-    int i=2;
+    int i=3;
     long long int sum;
     do
     {
         if (isPrime(i) == true)
         {
-            sum += i;
+            primeList.push_back(i);
             std::cout << i << std::endl;
         }
-        i++;        
+        i += 2;        
     } while (i < n);
+
+    for (auto prime : primeList)
+    {
+        sum += prime;
+    }  
     return sum;
 }
 
 int main()
 {
-    std::cout << sumPrimes(1000) << std::endl;
+    primeList.push_back(2);
+    std::cout << sumPrimes(2000000) << std::endl;
     return 0;
 }
